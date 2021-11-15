c52863586e02194156787e5127c3263075f6466f
huwenfan <huhuang03@gmail.com>
2021-11-15 19:46:16 +0800

    up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import * as babylon from "babylon";
+// import * as babylon from "babylon";
 
-const code = `function square(n) {
-  return n * n;
-}`;
-
-babylon.parse(code);
+// const code = `function square(n) {
+//   return n * n;
+// }`;
+//
+// babylon.parse(code);
 
 
 export default function ({types: t}) {
     return {
         visitor: {
-          BinaryExpression(path) {
-              if (path.node.operator !== "===") {
-                  return;
-              }
-              path.node.left = t.identifier("sebmck");
-              path.node.right = t.identifier("dork");
+          Identifier(path) {
+            if (path.node.name === 'foo') {
+              path.node.name = 'bar';
+            }
           }
+          // BinaryExpression(path) {
+          //     if (path.node.operator !== "===") {
+          //         return;
+          //     }
+          //     path.node.left = t.identifier("sebmck");
+          //     path.node.right = t.identifier("dork");
+          // }
         }
     }
 }
